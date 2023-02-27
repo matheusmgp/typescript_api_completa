4e4491d0f266f90e52bf91abc92bb1f6ab6fec62
Matheus Gustavo <matheus_mgp@hotmail.com>
2023-02-27 16:59:26 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("test");
+import express from "express";
+import { config } from "dotenv";
+
+const app = express();
+
+const port = process.env.PORT || 3333;
+
+app.get("/test", (req, res) => {
+  res.status(200).json("WORKED!!!");
+});
+
+app.listen(port, () => console.log("App running on port " + port));
